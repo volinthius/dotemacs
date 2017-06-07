e6604e4a5a86b4b0cd69943e9fa3c745fde1c120
Eino Malinen <eino.malinen@inoi.fi>
2017-06-08 00:00:04 +0300

    No font for non-windowed Emacs

--- init.d/1_face.el
+++ init.d/1_face.el
@@ -1,15 +1,19 @@
-(set-face-attribute 'default nil
-                    :font "DejaVu Sans Mono"
-                    :background "grey14"
-                    :foreground "white"
-                    )
+(if (window-system)
+    ((set-face-attribute 'default nil
+			:font "DejaVu Sans Mono"
+			:background "grey14"
+			:foreground "white"
+			)
+     (set-face-attribute 'mode-line nil
+			 :background "grey30"
+			 :foreground "grey80"
+			 )
 
-(set-face-attribute 'mode-line nil
-                    :background "grey30"
-                    :foreground "grey80"
-                    )
-
-(set-face-attribute 'cursor nil
-                    :background "lightblue"
-                    :foreground "black"
-                    )
+     (set-face-attribute 'cursor nil
+			 :background "lightblue"
+			 :foreground "black"
+			 ))
+  (set-face-attribute 'default nil
+			:background "grey14"
+			:foreground "white"
+			))
