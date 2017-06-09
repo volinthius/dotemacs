2149588e110cf0fa41deecfa52ad5db3778d0309
Eino Malinen <eino.malinen@inoi.fi>
2017-06-09 09:01:56 +0300

    Add melpa-stable

--- init.d/2_package.el
+++ init.d/2_package.el
@@ -1,6 +1,7 @@
 (require 'package)
 (add-to-list 'package-archives
-	     '("melpa" . "http://melpa.org/packages/"))
+	     '("melpa" . "http://melpa.org/packages/")
+	     '("melpa-stable" . "http://melpa-stable.org/packages/"))
 (package-initialize)
 
 (require 'use-package)
