076423426081e8420344bf03acfd85bd63942c36
Eino Malinen <eino.malinen@inoi.fi>
2017-07-19 10:31:54 +0300

    Differentiate face for Windows

--- init.d/1_face.el
+++ init.d/1_face.el
@@ -1,19 +1,35 @@
-(if (window-system)
-    (progn (set-face-attribute 'default nil
-			:font "DejaVu Sans Mono"
-			:background "grey14"
-			:foreground "white"
-			)
-     (set-face-attribute 'mode-line nil
-			 :background "grey30"
-			 :foreground "grey80"
-			 )
+(cond ((eq window-system 'w32)
+       (progn (set-face-attribute 'default nil
+                                  :background "grey14"
+                                  :foreground "white"
+                                  )
+              (set-face-attribute 'mode-line nil
+                                  :background "grey30"
+                                  :foreground "grey80"
+                                  )
 
-     (set-face-attribute 'cursor nil
-			 :background "lightblue"
-			 :foreground "black"
-			 ))
-  (progn (set-face-attribute 'default nil
-			:background "grey14"
-			:foreground "white"
-			)))
+              (set-face-attribute 'cursor nil
+                                  :background "lightblue"
+                                  :foreground "black"
+                                  )))
+      ((eq window-system 'x)
+       (progn (set-face-attribute 'default nil
+                                  :font "DejaVu Sans Mono"
+                                  :background "grey14"
+                                  :foreground "white"
+                                  )
+              (set-face-attribute 'mode-line nil
+                                  :background "grey30"
+                                  :foreground "grey80"
+                                  )
+
+              (set-face-attribute 'cursor nil
+                                  :background "lightblue"
+                                  :foreground "black"
+                                  )))
+       ((eq window-system nil)
+        (progn (set-face-attribute 'default nil
+                                   :background "grey14"
+                                   :foreground "white"
+                                   ))))
+
