7f0c686ae8164827f107ebf819940a1549a72c8f
Eino Malinen <eino.malinen@iki.fi>
2018-06-14 22:32:58 +0300

    Disable company-jedi and use jedi w/ jedi-direx

--- init.d/6_company.el
+++ init.d/6_company.el
@@ -1,9 +1,9 @@
-(use-package company
-  :ensure t
-  :init
-  (add-hook 'after-init-hook 'global-company-mode)
-  (bind-key (kbd "M-N") 'company-select-next)
-  (bind-key (kbd "M-P") 'company-select-previous))
-
-(use-package company-jedi
-  :ensure t)
+;(use-package company
+;  :ensure t
+;  :init
+;  (add-hook 'after-init-hook 'global-company-mode)
+;  (bind-key (kbd "M-N") 'company-select-next)
+;  (bind-key (kbd "M-P") 'company-select-previous))
+;
+;(use-package company-jedi
+;  :ensure t)
--- init.d/6_python.el
+++ init.d/6_python.el
@@ -1,3 +1,14 @@
 (use-package virtualenvwrapper
   :ensure t
   :init (setq venv-location "~/virtualenv"))
+
+(use-package jedi
+  :ensure t
+  :init
+  (add-hook 'python-mode-hook 'jedi:setup))
+
+(use-package jedi-direx
+  :ensure t
+  :init
+  (bind-key (kbd "C-c x") 'jedi-direx:pop-to-buffer)
+  (add-hook 'jedi-mode-hook 'jedi-direx:setup))
