dbed5599fdd8f9d7eb26d07d28ee63de22269830
Eino Malinen <eino.malinen@inoi.fi>
2019-09-17 10:20:35 +0300

    Remove jedi-direx

--- init.d/6_python.el
+++ init.d/6_python.el
@@ -6,9 +6,3 @@
   :ensure t
   :init
   (add-hook 'python-mode-hook 'jedi:setup))
-
-(use-package jedi-direx
-  :ensure t
-  :init
-  (bind-key (kbd "C-c x") 'jedi-direx:pop-to-buffer)
-  (add-hook 'jedi-mode-hook 'jedi-direx:setup))
