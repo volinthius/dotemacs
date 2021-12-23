6ef839be04dfb347e0d238f6b5b74765f1eab860
Eino Malinen <eino.malinen@iki.fi>
2021-12-23 19:17:08 +0200

    Remove go support

--- init.d/6_go.el
+++
@@ -1,7 +0,0 @@
-(use-package go-mode
-  :ensure t
-  :mode "\\.go$")
-
-(if (executable-find "go")
-    (start-process "go-get-goflymake"
-                   nil "go" "get" "-u" "github.com/dougm/goflymake"))
