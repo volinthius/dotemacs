1f188e3c067608d35977d05b6fc169d781c17c3c
Eino Malinen <eino.malinen@iki.fi>
2021-12-23 19:16:43 +0200

    Reconfigure flycheck

--- init.d/6_flycheck.el
+++ init.d/6_flycheck.el
@@ -9,18 +9,11 @@
 (use-package flycheck
   :ensure t
   :init
-  (global-flycheck-mode)
-  (flycheck-add-next-checker 'python-flake8 'python-pylint))
+  (global-flycheck-mode))
+;  (flycheck-add-next-checker 'python-flake8 'python-pylint)
+;  (flycheck-add-next-checker 'python-flake8 'python-mypy))
 
-(add-to-list
- 'load-path
- (concat (getenv "GOPATH") "/src/github.com/dougm/goflymake/"))
-(require 'go-flymake)
 
-(add-to-list
- 'load-path
- (concat (getenv "GOPATH") "/src/github.com/dougm/goflymake/"))
-(require 'go-flycheck)
 
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars unresolved)
